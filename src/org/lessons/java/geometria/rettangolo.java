8773bd52e99c6fa377fec6ae359ac2bd8c084c57
AndreaGiacometti <159144064+AndreaGiacometti@users.noreply.github.com>
2024-04-22 19:52:13 +0200

    Aggiunto metodi per calcolo area e perimetro

--- src/org/lessons/java/geometria/rettangolo.java
+++ src/org/lessons/java/geometria/rettangolo.java
@@ -3,5 +3,14 @@ package org.lessons.java.geometria;
 public class rettangolo {
 	int base;
 	int altezza;
+	
+	public int calcolaArea(int numero1, int numero2) {
+		int area = base * altezza;
+		return area;
+	}
+	
+	public int calcolaPerimetro(int numero1, int numero2) {
+		int perimetro = (base * 2) + (altezza * 2);
+		return perimetro;
 	}
 }
