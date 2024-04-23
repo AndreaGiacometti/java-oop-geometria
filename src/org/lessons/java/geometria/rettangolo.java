15ce2d3720dcac25caec28b99029cf7a9dca3205
AndreaGiacometti <159144064+AndreaGiacometti@users.noreply.github.com>
2024-04-23 18:23:21 +0200

    Risolto errori per classe Rettangolo

--- src/org/lessons/java/geometria/rettangolo.java
+++ src/org/lessons/java/geometria/rettangolo.java
@@ -1,16 +1,16 @@
 package org.lessons.java.geometria;
 
-public class rettangolo {
-	int base;
-	int altezza;
-	
-	public int calcolaArea(int numero1, int numero2) {
-		int area = base * altezza;
-		return area;
+public class Rettangolo {
+		int base;
+		int altezza;
+
+		int calcolaArea() {
+			int area = base * altezza;
+			return area;
+		}
+		
+		int calcolaPerimetro() {
+			int perimetro = (base * 2) + (altezza * 2);
+			return perimetro;
+		}
 	}
-	
-	public int calcolaPerimetro(int numero1, int numero2) {
-		int perimetro = (base * 2) + (altezza * 2);
-		return perimetro;
-	}
-}
